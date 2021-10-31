f59bb07cf19ddb3898e7b91b6e02d718e33f6019
Johnny Oshika <johnnyoshika@gmail.com>
2021-10-30 20:31:51 -0700

    Test blockchain mutation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Block, Blockchain } from 'Blockchain';
 
 const blockChain = new Blockchain();
-blockChain.addBlock(
-  new Block(Date.now().toString(), {
-    from: 'John',
-    to: 'Bob',
-    amount: 100,
-  }),
-);
+blockChain.addBlock({
+  from: 'John',
+  to: 'Bob',
+  amount: 100,
+});
 console.log(blockChain.chain);
-console.log(blockChain.isValid());
+console.log(`Is Valid: ${blockChain.isValid()}`);
+
+console.log('-----------------TAMPER DATA-----------------');
+
+blockChain.chain[1].data = { bad: 'data' };
+// This step and all following chains (if there were any) are necessary to make blockChain valid again, but it would be so expensive to make it impractical
+blockChain.chain[1].hash = blockChain.chain[1].getHash();
+console.log(blockChain.chain);
+console.log(`Is Valid: ${blockChain.isValid()}`);
